4050fb6b21013fada704380e573d38f4dbfa6177
amirrezarezai <amirreza568834@gmail.com>
2023-03-03 14:18:12 +0330

    added view and changed another part

---
+++ posts/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Post
+
+
+class PostSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
+        read_only_fields = ('id', 'created_at','updated_at')
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from .models import Post
+from .serializers import PostSerializers
+from rest_framework import generics, permissions
 # Create your views here.
+
+
+class PostList(generics.ListCreateAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializers
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializers
